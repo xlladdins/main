fa23fec8230c7eddf60c34585204dc76e9dac2f2
Keith A. Lewis <keithalewis@live.com>
2019-04-20 22:08:36 -0400

    Use more efficient routines for take

--- xllarray/xllarray.h
+++ xllarray/xllarray.h
@@ -13,7 +13,7 @@
 namespace xll {
 
     // Return pointer to OPER if o is a handle, otherwise return a nullptr
-    inline LPOPER is_handle(OPER& o)
+    inline LPOPER is_handle(const OPER& o)
     {
         if (o.size() == 1 && o[0].xltype == xltypeNum) {
             handle<OPER> ho(o[0].val.num);
@@ -25,26 +25,45 @@ namespace xll {
     }
 
     template<class O>
-    inline O& take(O& o, int n)
+    inline void take(O& o, int n)
     {
         if (abs(n) <= o.size()) {
             if (n > 0) {
                 o.resize(n, o.columns());
             }
             else if (n < 0) {
-                // copy_backward is more efficient, if it works
-                std::reverse(o.begin(), o.end());
+                std::copy(o.end() + n*o.columns(), o.end(), o.begin());
                 o.resize(-n, o.columns());
-                std::reverse(o.begin(), o.end());
             }
             else {
                 o.resize(0,0);
             }
         }
+    }
+    template<class O>
+    inline O take(const O& co, int n)
+    {
+        OPER o;
+
+        if (abs(n) <= co.size()) {
+            if (n > 0) {
+                o.resize(n, o.columns());
+                std::copy(co.begin(), co.begin() + n*co.columns(), o.begin());
+            }
+            else if (n < 0) {
+                o.resize(-n, o.columns());
+                std::copy(co.begin() + (co.rows() + n)*co.columns(), co.end(), o.begin());
+            }
+            else {
+                o.resize(0, 0);
+            }
+        }
+        else {
+            o = co;
+        }
 
         return o;
     }
-
     template<class O>
     inline O& drop(O& o, int n)
     {
--- xllarray/xlltake.cpp
+++ xllarray/xlltake.cpp
@@ -1,16 +1,22 @@
 // xlltake.cpp - Take elements from the front or back of a range.
+#include "../xll12/xll/shfb/entities.h"
 #include "xllarray.h"
 
 using namespace xll;
 
 AddIn xai_range_take(
     Function(XLL_LPOPER, L"?xll_range_take", PREFIX L"RANGE.TAKE")
-    .Arg(XLL_LONG, L"n", L"is the number of elements to take from the front (n &gt; 0) or back (n &lt; 0) of the range.")
+    .Arg(XLL_LONG, L"n", L"is the number of elements to take from the front rows (n positive) or back rows (n negative) of a range.")
     .Arg(XLL_LPOPER, L"range", L"is a range or a handle to a range.")
     .FunctionHelp(L"Return a new range or modify the range if a handle is given.")
-    .Documentation(LR"(Documentation goes here.)")
+    .Category(CATEGORY)
+    .Documentation(LR"xyz(
+If the absolute value of <codeInline>n</codeInline> is greater than or equal to the number of rows of the
+range the operation returns the entire range. If <codeInline>n</codeInline> is 0 the result is
+<codeInline>xltypNil</codeInline>: the Excel notion of an empty range.
+)xyz")
 );
-LPOPER WINAPI xll_range_take(LONG n, LPOPER pr)
+LPOPER WINAPI xll_range_take(LONG n, OPER const* pr)
 {
 #pragma XLLEXPORT
     static OPER o;
@@ -34,5 +40,56 @@ LPOPER WINAPI xll_range_take(LONG n, LPOPER pr)
 }
 
 #ifdef _DEBUG
+
+test test_range_take([] {
+    {
+        OPER o{OPER(1), OPER(true), OPER(L"a")};
+        ensure(o.size() == 3);
+        take(o, 0);
+        ensure(o.size() == 0);
+        ensure(o.xltype == xltypeNil);
+    }
+    {
+        const OPER o{OPER(1), OPER(true), OPER(L"a")};
+        ensure (o.size() == 3);
+        OPER o1 = take(o, 0);
+        ensure(o.size() == 3);
+        ensure (o1.xltype == xltypeNil);
+    }
+    {
+        OPER o{OPER(1), OPER(true), OPER(L"a")};
+        o.resize(o.size(), 1);
+        ensure(o.size() == 3);
+        take(o, 2);
+        ensure(o.size() == 2);
+    }
+    {
+        OPER o_{OPER(1), OPER(true), OPER(L"a")};
+        o_.resize(o_.size(), 1);
+        const OPER o{o_};
+        ensure(o.size() == 3);
+        OPER o1 = take(o, 2);
+        ensure(o.size() == 3);
+        ensure(o1[0] == o[0] && o1[1] == o[1]);
+    }
+    {
+        OPER o{OPER(1), OPER(true), OPER(L"a")};
+        o.resize(o.size(), 1);
+        ensure(o.size() == 3);
+        take(o, -2);
+        ensure(o.size() == 2);
+        ensure(o[0] == true);
+        ensure(o[1] == OPER(L"a"));
+    }
+    {
+        OPER o_{OPER(1), OPER(true), OPER(L"a")};
+        o_.resize(o_.size(), 1);
+        const OPER o{o_};
+        ensure(o.size() == 3);
+        OPER o1 = take(o, -2);
+        ensure(o.size() == 3);
+        ensure(o1[0] == o[1] && o1[1] == o[2]);
+    }
+});
 
 #endif // _DEBUG
